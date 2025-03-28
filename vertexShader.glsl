ec048e67276e7f32ff7a106f60b47dc35d9db162
kimodoorange <kimodoorange@gmail.com>
2025-03-28 18:32:45 -0500

    Update vertexShader.glsl

--- vertexShader.glsl
+++ vertexShader.glsl
@@ -1,4 +1,5 @@
 attribute vec4 position;
+
 uniform float time;
 uniform float formFluidity;
 
@@ -9,10 +10,13 @@ void main() {
     vUv = position.xy * 0.5 + 0.5;
     vTime = time;
 
-    // Dynamic vertex displacement based on form fluidity
-    vec4 displacedPosition = position;
-    float distortion = sin(time * formFluidity) * 0.2;
-    displacedPosition.xy += distortion * position.yx;
+    float angle = time * 0.3;
+    float twist = sin(time * 0.5 + position.x * 3.0) * formFluidity * 0.2;
 
-    gl_Position = displacedPosition;
+    mat2 rotation = mat2(cos(angle + twist), -sin(angle + twist),
+                         sin(angle + twist),  cos(angle + twist));
+
+    vec2 transformed = rotation * position.xy;
+
+    gl_Position = vec4(transformed, 0.0, 1.0);
 }
