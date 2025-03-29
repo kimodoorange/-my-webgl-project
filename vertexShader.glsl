58b867ad6d46f19f3f248325de729a5cb4e701e0
kimodoorange <kimodoorange@gmail.com>
2025-03-28 22:27:49 -0500

    Update vertexShader.glsl

--- vertexShader.glsl
+++ vertexShader.glsl
@@ -10,14 +10,10 @@ void main() {
     vUv = position.xy * 0.5 + 0.5;
     vTime = time;
 
-    // Convert to polar coordinates
     float r = length(position.xy);
     float theta = atan(position.y, position.x);
-
-    // Spiral transformation
     float spiral = time * 0.4 + r * 4.0;
     theta += sin(spiral) * 0.2 * formFluidity;
-
     vec2 spiralPos = vec2(cos(theta), sin(theta)) * r;
 
     gl_Position = vec4(spiralPos, 0.0, 1.0);
